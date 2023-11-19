7094baa4eab625e8093336ec28603b90bd10145b
ki11ean <ki11eanprog@gmail.com>
2023-11-19 19:53:20 +0300

    second commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,5 @@
 plugins {
+    application
     id("java")
 }
 
@@ -9,11 +10,19 @@ repositories {
     mavenCentral()
 }
 
+application {
+    mainClass = "hexlet.code.App"
+}
+
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
 }
 
 tasks.test {
     useJUnitPlatform()
 }
+
+//compileJava {
+//    options.release = 21
+//}
