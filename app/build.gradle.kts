63b769cb01154c27b33106e6725718adc6646a1e
ki11ean <ki11eanprog@gmail.com>
2023-12-03 18:28:01 +0300

    upd build.gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
     checkstyle
     id("jacoco")
     id("java")
+    id("se.patrikerdes.use-latest-versions") version "0.2.18"
+    id("com.github.ben-manes.versions") version "0.41.0"
 }
 
 group = "hexlet.code"
@@ -40,12 +42,10 @@ tasks.jacocoTestReport {
     }
 }
 
-java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(19))
-    }
-}
-
 tasks.getByName("run", JavaExec::class) {
     standardInput = System.`in`
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.release.set(20)
 }
